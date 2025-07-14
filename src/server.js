a2656fb190b8559c850bce22d44cbadb5c93ce7b
aldairandre99 <nzolageral@gmail.com>
2025-07-14 17:36:47 +0100

    feat: add files

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+const consultas = [];
+const PORT = process.env.PORT 
+
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
+
+app.get("/consultas", (req, res) => {
+  res.json(consultas);
+});
+
+app.post("/consultas", (req, res) => {
+  const consulta = req.body;
+  consultas.push(consulta);
+  res.status(201).json({ message: "Consulta agendada!", consulta });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
